1db62e3bb06d3198a2e73beffd0f04dff64d34ca
Anthony DeStefano <adx@fastmail.fm>
2019-08-06 16:50:45 -0400

    Clock

---
+++ src/bin/clock.rs
@@ -0,0 +1,42 @@
+use raytra::canvas::Canvas;
+use raytra::tup::Point;
+use raytra::matrix::Matrix4x4;
+use raytra::color;
+
+use std::f32::consts::PI;
+
+fn color(c:&mut Canvas, x: usize, y: usize) {
+    c.write_at(x-1, y+1, color::RED);
+    c.write_at(x, y+1, color::RED);
+    c.write_at(x+1, y+1, color::RED);
+
+    c.write_at(x-1, y, color::RED);
+    c.write_at(x, y, color::RED);
+    c.write_at(x+1, y, color::RED);
+
+    c.write_at(x-1, y-1, color::RED);
+    c.write_at(x, y-1, color::RED);
+    c.write_at(x+1, y-1, color::RED);
+}
+
+fn main() {
+    const CANVAS_SIZE: usize = 400;
+
+    let mut c = Canvas::new(CANVAS_SIZE, CANVAS_SIZE);
+
+    let rot = Matrix4x4::IDENTITY.rotate_z(PI / 6.0);
+    let t = Matrix4x4::IDENTITY.scale(150.0, 150.0, 0.0)
+        .translate(200.0, 200.0, 0.0);
+
+    let mut p = Point::new(0.0, 1.0, 0.0);
+
+    for _i in 0..12 {
+        p = rot * p;
+
+        let x = (t * p).x() as usize;
+        let y = (t * p).y() as usize;
+        color(&mut c, x, y);
+    }
+
+    println!("{}", c.to_ppm());
+}
